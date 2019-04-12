6eacce16e39d751b105cf3db25bf7dbf7950934e
visualstudio <visualstudio@122-PC19>
2019-04-12 18:14:54 -0400

    Assignment5-C0730069

--- Assignment 5 - C0730069/Program.cs
+++ Assignment 5 - C0730069/Program.cs
@@ -7,9 +7,30 @@ using System.Threading.Tasks;
 namespace Assignment_5___C0730069
 {
     class Program
-    {
-        static void Main(string[] args)
+  
         {
+            string[] Letters = new string[26] { "a", "b", "c", "d", "e", "f", "g", "i", "j", "k", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+            public static void Main()
+            {
+                Program orange = new Program();
+                Console.WriteLine(orange.Gematria("universe"));
+            }
+            public int Gematria(string word)
+            {
+                int GematriaValue = 0;
+                for (int i = 0; i < word.Length; i++)
+                { GematriaValue += LetterValue(word[i].ToString()); }
+                return GematriaValue;
+            }
+            public int LetterValue(string letter)
+            {
+                int x = 0;
+                while (Letters[x] != letter)
+                {
+                    if (Letters[x++] == letter) return x;
+                }
+                return -1;
+            }
         }
     }
-}
+
